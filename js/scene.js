6882771a87e8d4f020d3ffc407a5049522fe0cb5
YounsooLim <tomandlim@gmail.com>
2026-03-13 23:23:06 +0900

    perf: set pixelRatio to 1 for reduced GPU fragment load

--- js/scene.js
+++ js/scene.js
@@ -34,7 +34,7 @@ export class MMDScene {
       canvas: this.canvas,
       antialias: true,
     });
-    this.renderer.setPixelRatio(window.devicePixelRatio);
+    this.renderer.setPixelRatio(1);
     this.scene.background = new Color(0x1a1a1a);
 
     await this.renderer.init();
